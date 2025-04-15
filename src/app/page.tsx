815927130e87b954dde990d4ead142d03032c119
rajwindersxxx <rajwindersxxx@gmail.com>
2025-04-15 18:24:04 +0530

    forget to remove imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import MovingLogo from "./components/MovingLogo";
+// import MovingLogo from "./components/MovingLogo";
 import SessionCallToAction from "./components/SessionCallToAction";
 import SessionContact from "./components/SessionContact";
 import SessionService from "./components/SessionService";
